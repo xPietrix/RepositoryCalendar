3428919ea90f0020ada4aeee79d805ffd3752be8
Piotr Wasiak <xpiotr.wasiakx@gmail.com>
2018-06-03 17:20:23 +0200

    Interfejs
    Już sie nie zamyka główne okno

--- komponentowe/Calendar/src/main/java/PiotrFilip/Calendar/AddEventWindow.java
+++ komponentowe/Calendar/src/main/java/PiotrFilip/Calendar/AddEventWindow.java
@@ -31,8 +31,7 @@ public class AddEventWindow extends JFrame
 	{
 		this.XMLService = XMLService;
 		this.SQLService = SQLService;
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
 		setBounds(100, 100, 592, 519);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
